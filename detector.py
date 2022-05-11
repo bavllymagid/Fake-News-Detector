1fa406987d0eab445fa833bc6a4e61fe0a4f92fd
bavlly <pavllymagid04@gmail.com>
2022-05-11 04:32:14 +0200

    added mean squared error

--- detector.py
+++ detector.py
@@ -5,9 +5,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from preprocessing import *
 from nltk.stem import PorterStemmer
-
-
-
+from sklearn import metrics
 
 data = pd.read_csv('news.csv')
 
@@ -31,12 +29,12 @@ word_model = Word2Vec(filtered_sentence, min_count = 1,
                       window = 5, sg = 1)
 word_vectors = np.zeros((len(data), 100))
 word_vectors = vectors_build(word_vectors, filtered_sentence, word_model, len(data))
-x0_train, x0_test, y0_train, y0_test = train_test_split(word_vectors, y, test_size=0.3, random_state=42)
-model = LogisticRegression().fit(x0_train, y0_train)
-y0_pred = model.predict(x0_test)
+X_train, X_test, Y_train, Y_test = train_test_split(word_vectors, y, test_size=0.3, random_state=42)
+model = LogisticRegression().fit(X_train, Y_train)
+y_pred = model.predict(X_test)
 
-print("Word2Vec, original:")
-new_report(y0_test, y0_pred)
+print("mean squared error: " , metrics.mean_squared_error(Y_test, y_pred))
+new_report(Y_test, y_pred)
 
 
 
